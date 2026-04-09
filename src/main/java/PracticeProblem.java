7011267463dcd89201e837cf787853762d6d355c
Kaveeshan-Sathasivam <343279329@tdsb.ca>
2026-04-09 15:27:31 +0000

    jjandandawd

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,49 @@
+/*File: Practice Problem 4-2
+Author: Kaveeshan Sathasivam
+Date Created: Apr 9, 2026
+Date Modified: Apr 9, 2026
+ */
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	
+	public static String repeatLetter(char letter, int times) {
+        String result = "";
+
+        for (int i = 0; i < times; i++) {
+            result = result + letter;
+        }
+
+        return result;
+    }
+
+    public static int countLetter(String text, char letter) {
+        int count = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == letter) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static int evenSum(int num1, int num2) {
+        int sum = 0;
+
+        int start = Math.min(num1, num2);
+        int end = Math.max(num1, num2);
+
+        for (int i = start; i <= end; i++) {
+            if (i % 2 == 0) { 
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
 
 }
